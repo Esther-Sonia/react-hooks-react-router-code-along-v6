5e10454e1270ed725ada31b10be758a00cfda492
Esther-Sonia <esthersonia21@gmail.com>
2025-04-16 15:59:01 +0300

    Set up React Router with createBrowserRouter and RouterProvider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import UserProfile from "./pages/UserProfile";
+import ErrorPage from "./pages/ErrorPage";
+import routes from "./routes.js";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />);
+
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+        errorElement: <ErrorPage />
+      }, 
+      {
+        path: "/about",
+        element: <About />,
+        errorElement: <ErrorPage />
+      },
+      {
+        path: "/login",
+        element: <Login />,
+        errorElement: <ErrorPage />
+      },
+      {
+        path: "/profile/:id",
+        element: <UserProfile />,
+        errorElement: <ErrorPage />
+      }
+    ]);
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(<RouterProvider router={router} />);
